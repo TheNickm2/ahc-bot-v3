e8dbfc3972c4334f5c35ab9c6b1d12eb2cbc8390
Nick Mykkanen <th3nickm2@gmail.com>
2024-06-13 17:37:09 -0500

    server booster button

--- src/interaction-handlers/serverBoosters.ts
+++ src/interaction-handlers/serverBoosters.ts
@@ -8,10 +8,23 @@ import { Constants } from '../config/constants';
 })
 export class ButtonHandler extends InteractionHandler {
 	public async run(interaction: ButtonInteraction) {
-		await interaction.reply({
-			content: 'Hello from a button interaction handler!',
-			// Let's make it so only the person who pressed the button can see this message!
-			ephemeral: true,
+		if (!interaction.inGuild) return;
+		await interaction.deferReply({ ephemeral: true });
+		const guild = interaction.guild;
+		await guild?.members.fetch();
+		await guild?.roles.fetch();
+		const role = guild?.roles.premiumSubscriberRole;
+		if (!role) {
+			return await interaction.editReply({
+				content: 'An error occurred while fetching the server boosters role. Please try again later.',
+			});
+		}
+		const memberList = role.members.sort((a, z) => (a.displayName > z.displayName ? 1 : -1));
+		const boostersListString = memberList.reduce((acc, member) => {
+			return `${acc}\n${Constants.EMOTES.SERVER_BOOST} ${member.toString()}`;
+		}, '');
+		return await interaction.editReply({
+			content: `HUGE THANK YOU to our server boosters, listed below:${boostersListString}\n\nMany of the great features of our Discord server would be impossible to use without the support of these amazing server boosters!`,
 		});
 	}
 
